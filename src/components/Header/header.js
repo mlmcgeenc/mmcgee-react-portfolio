474b01c1fa644793045ee7f7de6432fd13d58eb4
Matt McGee <mlmcgeenc@gmail.com>
2022-08-23 14:38:23 -0400

    build component folder structure

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Navigation/nav';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Nav />
+    </div>
+  );
+}
+
+export default Header;
