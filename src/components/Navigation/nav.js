bd1afa632cf9768fc56b81e145e168b1b0e73465
Matt McGee <mlmcgeenc@gmail.com>
2022-08-24 17:08:55 -0400

    add logo and nav item .active and :over colors

--- src/components/Navigation/nav.js
+++ src/components/Navigation/nav.js
@@ -6,7 +6,8 @@ const Navigation = () => {
 	return (
 		<nav className='navbar navbar-expand-lg navbar-fonts '>
 			<div className='container'>
-				<a className='navbar-brand' href='#'>
+        <div className="nav-logo"></div>
+				<a className='navbar-brand' href='#About'>
 					Matt McGee
 				</a>
 				<button
@@ -28,7 +29,7 @@ const Navigation = () => {
 							</a>
 						</li>
 						<li className='nav-item dropdown'>
-							<a className='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+							<a className='nav-link dropdown-toggle' href='#Portfolio' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
 								Portfolio
 							</a>
 							<ul className='dropdown-menu'>
@@ -41,22 +42,22 @@ const Navigation = () => {
 									<hr className='dropdown-divider'></hr>
 								</li>
 								<li>
-									<a className='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#Local-Music-Scene'>
 										Local Music Scene
 									</a>
 								</li>
 								<li>
-									<a className='dropdown-item' href='#'>
+									<a className='dropdown-item' href='#Movie-Search'>
 										Movie Search
 									</a>
 								</li>
 							</ul>
 						</li>
 						<li className='nav-item'>
-							<a className='nav-link'>Contact</a>
+							<a className='nav-link' href='#Contact'>Contact</a>
 						</li>
 						<li className='nav-item'>
-							<a className='nav-link disabled'>Resume</a>
+							<a className='nav-link disabled' href='#Resume'>Resume</a>
 						</li>
 					</ul>
 				</div>
