41c34edcf3fdd59eb6731f3e5cc5c62c6b01ceb0
Matt McGee <mlmcgeenc@gmail.com>
2022-08-25 16:55:30 -0400

    add error messages to contact form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,96 +3,113 @@ import 'bootstrap';
 import '../../Assets/css/custom.css';
 
 const Contact = () => {
-	const [errorMessage, setErrorMessage] = useState('');
-	const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-	const { name, email, message } = formState;
+	const [errorType, setErrorType] = useState('');
+  const [errorMessageText, setErrorMessageText] = useState('');
+  const [formState, setFormState] = useState({ name:'', email:'', message:'' });
+  const { name, email, message} = formState
 
-	function handleChange(e) {
-		// if (e.target.name === 'email') {
-		// 	const isValid = validateEmail(e.target.value);
-		// 	console.log(isValid);
-		// 	if (!isValid) {
-		// 		setErrorMessage('Please provide a valid email');
-		// 	} else {
-		// 		setErrorMessage('');
-		// 	}
-		// } else {
-		// 	if (!e.target.value.length) {
-		// 		setErrorMessage(`${e.target.name} is required.`);
-		// 	} else {
-		// 		setErrorMessage('');
-		// 	}
-		// }
+  const handleClearError = (e) => {
+    setErrorType('')
+  }
 
-		if (!errorMessage) {
-			setFormState({ ...formState, [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    console.log('handleChange fired')
+    if (!e.target.value.length) {
+      console.log(e.target.name+'-error')
+      setErrorType(e.target.name + '-error');
+		} else {
+			console.log('else coming soon')
 		}
-		console.log('errorMessage:', errorMessage);
-	}
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		console.log(formState);
-	}
+    if(!errorType) {
+      setFormState({ ...formState, [e.target.name]: e.target.value })
+      setErrorMessageText(`All fields are required. Please enter your ${e.target.name} to continue.`)
+    }
+    console.log('Error: ', errorType)
+  }
 
-	console.log(formState);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form submitted:", formState)
+  }
 
 	return (
-			<section className='contact-form  glass-effect col-12 col-sm-10 col-md-8 col-xl-6 mx-auto'>
-				<h1 data-testid='contactTitle'>Contact Me</h1>
-				<hr />
-				<form>
-					<div className='form-group'>
-						<label htmlFor='inputName'>Name</label>
-						<input
-							type='text'
-							className='form-control'
-							defaultValue={name}
-							onBlur={handleChange}
-							id='inputName'
-							name='inputName'
-							placeholder='Enter your name'
-						></input>
-					</div>
-					<div class='form-group'>
-						<label htmlFor='inputEmail'>Email address</label>
-						<input
-							type='email'
-							className='form-control'
-							defaultValue={email}
-							onBlur={handleChange}
-							id='inputEmail'
-							name='inputEmail'
-							placeholder='Enter your email'
-						/>
-						<small id='emailHelp' className='form-text text-muted'>
-							I'll never share your email with anyone else.
-						</small>
-					</div>
-					<div className='form-group'>
-						<label htmlFor='message'>Message</label>
-						<textarea
-							className='form-control'
-							defaultValue={message}
-							onBlur={handleChange}
-							id='message'
-							name='message'
-							rows='5'
-							placeholder='Leave me a message'
-						></textarea>
-					</div>
-					{errorMessage && (
+		<section className='contact-form  glass-effect col-12 col-sm-10 col-md-8 col-xl-6 mx-auto'>
+			<h1 data-testid='contactTitle'>Contact Me</h1>
+			<hr />
+			<form>
+				<div className='form-group'>
+					<label htmlFor='inputName'>Name</label>
+					<input
+						type='text'
+						className='form-control'
+						defaultValue={name}
+						onClick={handleClearError}
+						onBlur={handleChange}
+						id='inputName'
+						name='name'
+						placeholder='Enter your name'
+					></input>
+					{errorType === 'name-error' && (
 						<div>
-							<p className='error-text'>{errorMessage}</p>
+							<p name='name-error' className='error-text'>
+								{errorMessageText}
+							</p>
 						</div>
 					)}
-					<row className='d-flex justify-content-around'>
-						<button data-testid='contactButton' className='btn btn-lg btn-primary col-6 mt-4' type='submit' onSubmit={handleSubmit}>
-							Submit
-						</button>
-					</row>
-				</form>
-			</section>
+				</div>
+				<div className='form-group'>
+					<label htmlFor='inputEmail'>Email address</label>
+					<input
+						type='email'
+						className='form-control'
+						defaultValue={email}
+						onClick={handleClearError}
+						onBlur={handleChange}
+						id='inputEmail'
+						name='email'
+						placeholder='Enter your email'
+					/>
+					<small id='emailHelp' className='form-text text-muted'>
+						I'll never share your email with anyone else.
+					</small>
+					{errorType === 'email-error' && (
+						<div>
+							<p name='email-error' className='error-text'>
+								{errorMessageText}
+							</p>
+						</div>
+					)}
+				</div>
+				<div className='form-group'>
+					<label htmlFor='message'>Message</label>
+					<textarea
+						className='form-control'
+						defaultValue={message}
+						onClick={handleClearError}
+						onBlur={handleChange}
+						id='message'
+						name='message'
+						rows='5'
+						placeholder='Leave me a message'
+					></textarea>
+					{errorType === 'message-error' && (
+						<div>
+							<p
+                name='message-error'
+                className='error-text'
+							>{errorMessageText}
+							</p>
+						</div>
+					)}
+				</div>
+				<row className='d-flex justify-content-around'>
+					<button data-testid='contactButton' className='btn btn-lg btn-primary col-6 mt-4' type='submit' onSubmit={handleSubmit}>
+						Submit
+					</button>
+				</row>
+			</form>
+		</section>
 	);
 };
 
