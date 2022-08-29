6e8f4167611ce3f33bc3d00318180d8c0e8f4421
Matt McGee <mlmcgeenc@gmail.com>
2022-08-29 12:53:41 -0400

    render tech stack icons programatically to project cards

--- src/components/Project/project.js
+++ src/components/Project/project.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../Assets/css/custom.css';
 import logo from '../../Assets/images/Circle-Logo.png';
-import html5 from '../../Assets/images/tech-logos/html5.png'
+import html5 from '../../Assets/images/tech-logos/html5.png';
 import css from '../../Assets/images/tech-logos/css3.png';
 import js from '../../Assets/images/tech-logos/js.png';
 import bootstrap from '../../Assets/images/tech-logos/bootstrap.png';
@@ -13,49 +13,85 @@ import mongoDB from '../../Assets/images/tech-logos/mongoDB.png';
 import handlebars from '../../Assets/images/tech-logos/handlebars.png';
 import react from '../../Assets/images/tech-logos/react.png';
 
-
 const Project = ({ project }) => {
 	const { name, screenshot, link, tech } = project;
 
-  const getObject = (entry) => {
-    console.log('Switch entry', entry.entry)
-    switch (entry.entry) {
+	const getObject = (entry) => {
+		console.log('Switch entry', entry.entry);
+		switch (entry.entry) {
 			case 'html':
-				return { name: 'html', backgroundImage: html5 };
+				return {
+					name: 'html',
+					backgroundImage: html5,
+					link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+				};
 			case 'css':
-				return { name: 'css', backgroundImage: css };
+				return {
+					name: 'css',
+					backgroundImage: css,
+					link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+				};
 			case 'javascript':
-				return { name: 'javascript', backgroundImage: js };
+				return {
+					name: 'javascript',
+					backgroundImage: js,
+					link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+				};
 			case 'jquery':
 				return { name: 'jquery' };
 			case 'bootstrap':
-				return { name: 'bootstrap', backgroundImage: bootstrap };
+				return {
+					name: 'bootstrap',
+					backgroundImage: bootstrap,
+					link: 'https://getbootstrap.com',
+				};
 			case 'express':
-				return { name: 'express', backgroundImage: express };
+				return {
+					name: 'express',
+					backgroundImage: express,
+					link: 'https://expressjs.com',
+				};
 			case 'mysql':
-				return { name: 'MySQL', backgroundImage: mysql };
+				return {
+					name: 'MySQL',
+					backgroundImage: mysql,
+					link: 'https://www.mysql.com',
+				};
 			case 'sequelize':
-				return { name: 'sequelize', backgroundImage: sequelize };
+				return {
+					name: 'sequelize',
+					backgroundImage: sequelize,
+					link: 'https://sequelize.org',
+				};
 			case 'handlebars':
-				return { name: 'handlebars', backgroundImage: handlebars };
+				return {
+					name: 'handlebars',
+					backgroundImage: handlebars,
+					link: 'https://handlebarsjs.com',
+				};
 			case 'mongodb':
-				return { name: 'mongodb', backgroundImage: mongoDB };
+				return {
+					name: 'mongodb',
+					backgroundImage: mongoDB,
+					link: 'https://www.mongodb.com',
+				};
 			case 'mongoose':
 				return { name: 'mongoose' };
 			case 'react':
-				return { name: 'react', backgroundImage: react };
+				return {
+					name: 'react',
+					backgroundImage: react,
+					link: 'https://reactjs.org',
+				};
 			default:
 				return { name: 'default' };
 		}
-  }
+	};
 
 	const StackIcon = (entry) => {
-    let object = getObject(entry)
-    console.log('StackIcon object:', object)
-    return (
-      <img src={`${object.backgroundImage}`}
-        className='mx-1'/>
-		);
+		let object = getObject(entry);
+		console.log('StackIcon object:', object);
+		return <a href={`${object.link}`} target="_blank"><img src={`${object.backgroundImage}`} className='mx-1 tech-icon' /></a>;
 	};
 
 	return (
