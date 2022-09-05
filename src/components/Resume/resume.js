7e2203ab0624650b6b304ecdeb535d088aedc9b4
Matt McGee <mlmcgeenc@gmail.com>
2022-09-04 21:18:57 -0400

    make resume downloadable

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import resume from '../../Assets/files/mcgee-resume.docx'
 
 const Resume = () => {
 	return (
 		<div>
-			<h1>Resume</h1>
+			<a href='../../Assets/files/mcgee-resume.docx' className='btn btn-lg btn-primary col-4 mt-4 p-3' download>Download My Resume</a>
 		</div>
 	);
 };
