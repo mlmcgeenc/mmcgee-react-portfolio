83b243c9f9979a0cff02e0414941b95591cfdb1f
Matt McGee <mlmcgeenc@gmail.com>
2022-09-04 21:02:16 -0400

    add correct screenshots for projects in porfolio page

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -3,12 +3,18 @@ import 'bootstrap'
 import '../../Assets/css/custom.css'
 import Project from '../Project/project'
 import screenshot from '../../Assets/images/screenshots/demo-screenshot.png'
+import localMusicScene from '../../Assets/images/screenshots/local-music-scene-screenshot.png'
+import ezStream from '../../Assets/images/screenshots/ez-stream-screenshot.png';
+import codeQuiz from '../../Assets/images/screenshots/code-quiz-screenshot.png';
+import weatherDashboard from '../../Assets/images/screenshots/weather-dashboard-screenshot.png';
+import simplePortfolio from '../../Assets/images/screenshots/simple-portfolio-screenshot.png';
+import employeeTracker from '../../Assets/images/screenshots/employee-tracker-screenshot.png';
 
 const Portfolio = () => {
   const projects = [
 		{
 			name: 'Local Music Scene',
-			screenshot: screenshot,
+			screenshot: localMusicScene,
 			repoLink: 'https://github.com/cpm-128/local-music-scene',
 			deployedLink: 'https://damp-plateau-16490.herokuapp.com/',
 			tech: [
@@ -24,35 +30,35 @@ const Portfolio = () => {
 		},
 		{
 			name: 'EZ Stream',
-			screenshot: screenshot,
+			screenshot: ezStream,
 			repoLink: 'https://github.com/mlmcgeenc/group-2-project-one',
 			deployedLink: 'https://mlmcgeenc.github.io/group-2-project-one/',
 			tech: ['html', 'css', 'javascript', 'foundation'],
 		},
 		{
 			name: 'Code Quiz',
-			screenshot: screenshot,
-			repoLink: 'https://github.com/mlmcgeenc/class-portfolio',
-			deployedLink: 'https://mlmcgeenc.github.io/class-portfolio/',
+			screenshot: codeQuiz,
+			repoLink: 'https://github.com/mlmcgeenc/js-quiz',
+			deployedLink: 'https://mlmcgeenc.github.io/js-quiz/',
 			tech: ['html', 'css', 'javascript'],
 		},
 		{
 			name: 'Weather Dashboard',
-			screenshot: screenshot,
-			repoLink: 'https://github.com/mlmcgeenc/js-quiz',
-			deployedLink: 'https://mlmcgeenc.github.io/js-quiz/index.html',
+			screenshot: weatherDashboard,
+			repoLink: 'https://github.com/mlmcgeenc/weather-dashboard',
+			deployedLink: 'https://mlmcgeenc.github.io/weather-dashboard/',
 			tech: ['html', 'css', 'javascript'],
 		},
 		{
 			name: 'Simple Portfolio',
-			screenshot: screenshot,
+			screenshot: simplePortfolio,
 			repoLink: 'https://github.com/mlmcgeenc/class-portfolio',
 			deployedLink: 'https://mlmcgeenc.github.io/class-portfolio/',
 			tech: ['html', 'css'],
 		},
 		{
 			name: 'Employee Tracker',
-			screenshot: screenshot,
+			screenshot: employeeTracker,
 			repoLink: 'https://github.com/mlmcgeenc/employee-tracker',
 			deployedLink: null,
 			tech: ['javascript', 'node'],
